6c5b554db56a44cb66a03bf15838752bbaff0981
tripdancer0916@gmail.com <ritalin202>
2018-09-03 14:03:31 +0900

    multi_layer2

---
+++ multi_layer2.py
@@ -0,0 +1,138 @@
+import numpy as np
+import chainer
+from chainer.backends import cuda
+from chainer import Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
+import PIL
+import os
+import matplotlib as mpl
+
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+os.makedirs('./result_ln6_2layer', exist_ok=True)
+
+# Load the MNIST dataset
+train, test = chainer.datasets.get_mnist()
+xs, ts = train._datasets
+txs, tts = test._datasets
+
+xs_9 = []
+ts_9 = []
+txs_9 = []
+tts_9 = []
+
+for i in range(ts.shape[0]):
+    if ts[i] != 9:
+        xs_9.append(xs[i])
+        ts_9.append(ts[i])
+
+for i in range(tts.shape[0]):
+    if tts[i] != 9:
+        txs_9.append(txs[i])
+        tts_9.append(tts[i])
+
+xs_9 = np.array(xs_9)
+ts_9 = np.array(ts_9)
+txs_9 = np.array(txs_9)
+tts_9 = np.array(tts_9)
+
+loop_n = 6
+
+
+# Network definition
+class SingleRNN(chainer.Chain):
+    def __init__(self, input_shape, hidden1, hidden2, classes):
+        super(SingleRNN, self).__init__(
+            l1=L.Linear(input_shape, hidden1),
+            r1=L.Linear(hidden1, hidden1),
+            l2=L.Linear(hidden1, hidden2),
+            r2=L.Linear(hidden2, hidden2),
+            l3=L.Linear(hidden2, classes)
+        )
+        self.hidden1 = hidden1
+        self.hidden2 = hidden2
+
+    def reset_state(self):
+        self.h1 = Variable(np.zeros((1, self.hidden1), dtype=np.float32))
+        self.h2 = Variable(np.zeros((1, self.hidden2), dtype=np.float32))
+
+    def __call__(self, x):
+        self.h1 = F.tanh(self.r1(self.h1) + self.l1(x))
+        for i in range(loop_n):
+            self.h1 = F.tanh(self.r1(self.h1))
+        self.h2 = F.tanh(self.r2(self.h2) + self.l2(self.h1))
+        for i in range(loop_n):
+            self.h2 = F.tanh(self.r2(self.h2))
+        y = self.l3(self.h2)
+        return y
+
+
+class Classifier(chainer.Chain):
+    def __init__(self, predictor):
+        super(Classifier, self).__init__(predictor=predictor)
+
+    def __call__(self, x, t):
+        y = self.predictor(Variable(x))
+        self.loss = F.softmax_cross_entropy(y, t)
+        for i in range(1, x.shape[0]):
+            y = self.predictor(x[i])
+            self.loss += F.softmax_cross_entropy(y, t[i])
+        return self.loss
+
+
+for k in range(100):
+    plt.figure()
+    singlernn = SingleRNN(784, 200, 200, 9)
+    # singlernn.reset_state()
+    model = Classifier(singlernn)
+    # Setup optimizer
+    optimizer = optimizers.SGD()
+    optimizer.setup(model)
+
+    for i in range(20000):
+        index = np.random.choice(range(50000))
+        singlernn.reset_state()
+        x = np.expand_dims(xs_9[index], axis=0)
+        t = np.expand_dims(ts_9[index], axis=0)
+        t = Variable(t)
+        model.cleargrads()
+        loss = model(x, t)
+        loss.backward()
+        optimizer.update()
+        loss.unchain_backward()
+
+    correct = 0
+    wrong = 0
+    for i in range(txs_9.shape[0]):
+        singlernn.reset_state()
+        x = np.expand_dims(txs_9[i], axis=0)
+        y = F.softmax(singlernn(x))
+        output = np.argmax(y.data[0])
+        if output == tts_9[i]:
+            correct = correct + 1
+        else:
+            wrong = wrong + 1
+    print(k, correct / (correct + wrong))
+
+    x = np.expand_dims(txs[12], axis=0)
+    output = []
+    singlernn.reset_state()
+    gamma = 0
+    for i in range(500):
+        y = singlernn(x)
+        output.append(y.data[0])
+    output = np.array(output)
+    for i in range(8):
+        if i == 4:
+            plt.plot(output.T[i][450:], label='output-4')
+        elif i == 7:
+            plt.plot(output.T[i][450:], label='output-7')
+        else:
+            plt.plot(output.T[i][450:])
+    plt.legend()
+    plt.savefig("./result_ln6_2layer/{}_ln6_2layer.png".format(k))
